cbac0deac1ffee6760c76cadc0b46544b003d15d
Sven <99063753@mydavinci.nl>
2021-11-12 15:25:39 +0100

    0.0.0.2 // page name finder version 1 finished
    -finished the first version of page finder

--- core/config.php
+++ core/config.php
@@ -1,13 +1,23 @@
 <?php
     //Settings
-    $websiteNaam = "Document";
+    const WEBSITENAME = "Document";
 
     //Functions
     function getPageName() {
-       $newArr = explode("/", $_SERVER['REQUEST_URI']);
-       $newArr[(count($newArr) - 1)] = str_replace(".php", "", $newArr[(count($newArr) - 1)]);
-       return $newArr[(count($newArr) - 1)];
+    
+    $newArr = explode("/", $_SERVER['REQUEST_URI']);
+    $newArr[(count($newArr) - 1)] = str_replace(".php", "", $newArr[(count($newArr) - 1)]);
+
+    if ($newArr[(count($newArr) - 1)] != null){
+        if ($newArr[(count($newArr) - 1)] != "index"){
+            return $newArr[(count($newArr) - 1)];
+        } else {
+            return "yes";
+        }
+    } else {
+        return WEBSITENAME;
     }
+}
 
 
 ?>
